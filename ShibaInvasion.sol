2f9f72ffe7e63d0549250464bfc14177962cc7d6
Augustin Bracco <135206977+QuantumScorpion@users.noreply.github.com>
2023-06-05 17:23:03 +0700

    Add files via upload

---
+++ ShibaInvasion.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.20;
+
+import "./shibaownership.sol";
+
+/** 
+ * @title The name of the game
+ * @author Augustin Bracco
+ */
+contract ShibaInvasion is ShibaOwnership
+    {
+
+    }
